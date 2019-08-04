1e3ee47663d29107762ea44b10b43672cb320bbf
ViableClanMember <viablegamer@protonmail.com>
2019-08-04 08:10:14 +0100

    fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,12 +13,12 @@ if [ ! -d "/bench/bench" ]; then
     sudo chmod 777 .
     bench init bench --frappe-path https://github.com/ViableClanMember/frappe --frappe-branch master
     cd bench
-    bench get-app erpnext https://github.com/frappe/erpnext --branch version-12 || echo "Failed to install erpnext app"
+    bench get-app erpnext https://github.com/frappe/erpnext --branch version-12
     bench update --patch || echo "Failed to update patch hack"
 fi;
 
 cp -f /home/frappe/common_site_config.json /bench/bench/sites/
-DB_PASS=$(cat /secrets/DB_PASS) sh -c 'bench config set-common-config -c root_password $DB_PASS'
+DB_PASS=$(cat /secrets/DB_PASS) sh -c 'cd /bench/bench && bench config set-common-config -c root_password $DB_PASS'
 ls -al
 
 # start supervisor
